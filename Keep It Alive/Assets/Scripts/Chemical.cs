ed5e1bdef84f570d0deb566a26c9ff910888f8f7
Lightyearz <lightyearz@iinet.net.au>
2020-04-18 19:27:51 +0930

    Mixturing

--- Keep It Alive/Assets/Scripts/Chemical.cs
+++ Keep It Alive/Assets/Scripts/Chemical.cs
@@ -2,10 +2,33 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum Effects
+{
+    Mutate,
+    Posion,
+    Drunk,
+    Happy,
+    Calm,
+    Anger,
+    Dizzy,
+    Nutrition,
+    Gas,
+    Plants,
+    Starve,
+    Bloat,
+    Pain
+}
+
 [CreateAssetMenu(fileName = "New Chemical", menuName = "Chemical", order = 1)]
 public class Chemical : Item
 {
     public bool Solid;
     public float PH;
     public Color InfluenceColor;
+
+    public float BurnTemperature;
+    public float FreezeTemperature;
+
+    public Effects Effect;
+    public int EffectIntensity;
 }
