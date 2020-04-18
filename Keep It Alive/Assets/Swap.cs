bf64a66e3bb88ca7bb505ddc2c303c8180b16619
AlbySchool <47169566+AlbySchool@users.noreply.github.com>
2020-04-18 13:11:50 +0930

    ieonvtnry setymm

--- Keep It Alive/Assets/Swap.cs
+++ Keep It Alive/Assets/Swap.cs
@@ -3,13 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-
-
-
-
-
-
-
 public class Swap : MonoBehaviour
 {
     public GameObject item;
@@ -20,7 +13,6 @@ public class Swap : MonoBehaviour
     public bool holding;
     public Sprite icon;
     public bool stackable;
-    public GameObject selectedUI;
 
     public void grabslot(GameObject vitem, string vitemname, string vdesription, int vamount, Sprite vicon, bool vstackable)
     {
@@ -31,20 +23,5 @@ public class Swap : MonoBehaviour
         holding = true;
         icon = vicon;
         stackable = vstackable;
-    }
-
-    public void Update()
-    {
-        if (holding)
-        {
-            selectedUI.SetActive(true);
-            selectedUI.GetComponentInChildren<Image>().sprite = icon;
-            selectedUI.GetComponentInChildren<Text>().text = amount.ToString();
-            selectedUI.transform.position = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
-        }
-        else
-        {
-            selectedUI.SetActive(false);
-        }
     }
 }
