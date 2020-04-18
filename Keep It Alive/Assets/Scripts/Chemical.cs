8f481333f51af87f295151832081f482f7dbdb0b
Lightyearz <lightyearz@iinet.net.au>
2020-04-18 17:48:22 +0930

    bruh bruh moment bruh momento bruj bruj bruj
    bruh funy dead mememe

--- Keep It Alive/Assets/Scripts/Chemical.cs
+++ Keep It Alive/Assets/Scripts/Chemical.cs
@@ -5,5 +5,7 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "New Chemical", menuName = "Chemical", order = 1)]
 public class Chemical : Item
 {
+    public bool Solid;
     public float PH;
+    public Color InfluenceColor;
 }
---
+++ Keep It Alive/Assets/Scripts/MixtureObject.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MixtureObject : MonoBehaviour
+{
+    public Chemical[] Chemicals;
+
+    public string Name;
+    public string Description;
+
+    public Color Color;
+
+    public string[] Effects;
+
+    public float PH;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public bool AddToMixture(Chemical Chemical)     //Returns false if failed
+    {
+        //Test if solid
+        if(Chemical.Solid == true)
+        {
+            if(PH < 5)  
+            {
+                //can dissolve solid
+            }
+            else
+            {
+                //cannot dissolve solid, not acidic enough
+                return false;
+            }
+        }
+        else
+        {
+
+        }
+    }
+
+    void RecalculateValues()
+    {
+
+    }
+}
+
+
